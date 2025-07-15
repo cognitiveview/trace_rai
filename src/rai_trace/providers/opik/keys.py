5accb453b40bf6f3c4dd5d126b252041b8d57fc4
Ashutosh Padhi <ashutosh@cognitiveview.com>
2025-07-15 14:42:02 +0530

    Keys for each evluation framework added, along with the common predictive metric keys

---
+++ src/rai_trace/providers/opik/keys.py
@@ -0,0 +1,15 @@
+OPIK_METRIC_KEYS = [
+    "Equals",
+    "Contains",
+    "RegexMatch",
+    "IsJson",
+    "SentenceBLEU",
+    "CorpusBLEU",
+    "ROUGE",
+    "Sentiment",
+    "Moderation",
+    "Usefulness",
+    "AnswerRelevance",
+    "ContextPrecision",
+    "ContextRecall"
+]
